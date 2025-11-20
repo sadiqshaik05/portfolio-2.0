260a695cc04582fde8fa3ada32a992734d034c41
sadiqshaik05 <shaiksadiq7525@gmail.com>
2025-11-20 23:56:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var typed = new Typed("#typed-name", {
+  strings: ["WELCOME TO MY PORTFOLIO"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true
+});
+var SS = new Typed("#SS-name", {
+  strings: ["HI !! THIS IS SADIQSHAIK"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true
+});
+var SK = new Typed("#SK-name", {
+  strings: ["I AM FULLSTACK WEB DEVELOPER","FRONTEND DESIGNER","PYTHON PROGRAMMER"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true
+});
+const accordions = document.querySelectorAll(".info-header");
+
+accordions.forEach(button => {
+    button.addEventListener("click", () => {
+        const content = button.nextElementSibling;
+
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+});
